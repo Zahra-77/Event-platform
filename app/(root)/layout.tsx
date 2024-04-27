f622105fd4aeaf32e9cebd1899a6a63cd8b7d7d3
Zahra Iranmanesh <zahra@Zahras-MBP.lan>
2024-04-27 13:31:31 -0700

    first commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,34 +1,17 @@
-// import type { Metadata } from "next";
-// import { Poppins } from "next/font/google";
-// import "./globals.css";
 
-import Header from "@/components/shared/Header";
-import { ClerkProvider } from "@clerk/nextjs";
-
-// const poppins = Poppins({ 
-//   subsets: ['latin'],
-//   weight: ['400', '500', '600', '700'],
-//   variable: '--font-poppins',
-//  });
-
-// export const metadata: Metadata = {
-//   title: "EventMaster",
-//   description: "Eventt Management System",
-//   viewport: "width=device-width, initial-scale=1",
-//   icons: {
-//   icon: "assests/images/logo.svg",
-//   }
-// };
+import Header from "@/components/shared/Header"
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <div className = "flex h-screen flex-col">
-        <Header />
-      <main className = "flex-1"> </main>
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+     
     </div>
   )
 }
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,71 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+// import type { Metadata } from "next";
+// import { Poppins } from "next/font/google";
+// import "./globals.css";
+// import { ClerkProvider } from "@clerk/nextjs";
+// import Header from "@/components/shared/Header";
 
-const poppins = Poppins({ 
+// const poppins = Poppins({ 
+//   subsets: ['latin'],
+//   weight: ['400', '500', '600', '700'],
+//   variable: '--font-poppins',
+//  });
+
+// export const metadata: Metadata = {
+//   title: "EventMaster",
+//   description: "Eventt Management System",
+//   viewport: "width=device-width, initial-scale=1",
+//   icons: {
+//     icon: "/assests/images/logo.svg",
+//   },
+// };
+
+
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <ClerkProvider>
+//     <html lang="en">
+//       <body className={poppins.variable}>{children}</body>
+//       <Header />
+//     </html>
+//    </ClerkProvider>
+//   );
+// }
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import './globals.css'
+
+const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
   variable: '--font-poppins',
- });
+})
 
 export const metadata: Metadata = {
-  title: "EventMaster",
-  description: "Eventt Management System",
-  //viewport: "width=device-width, initial-scale=1",
+  title: 'Event-Master',
+  description: 'Event-Master is a platform for event management.',
   icons: {
-    icon: "/assests/images/logo.svg",
-  },
-};
+    icon: '/assets/images/logo.svg'
+  }
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
-   </ClerkProvider>
-  );
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
 }
+
